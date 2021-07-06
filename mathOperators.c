0113778688935e009454795405c11bf531851d24
Ahsan Habib Sunny <ahsanhabibsunny@pop-os.localdomain>
2021-07-06 03:21:30 -0700

    Math Operators

---
+++ mathOperators.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int length = 20;
+    int width = 10;
+
+    /* Just Change Samble + ,- , *, / , and % .The Show Us Result */
+
+    int sum = length * width;
+    printf("The Length and Width is : %d",sum);
+}
